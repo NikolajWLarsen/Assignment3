2b2c8b11f368d763859c0a3e7d5a6d74e855ede2
Magnus Dalsgaard Larsen <45166031+BlueMango10@users.noreply.github.com>
2021-09-24 13:51:03 +0200

    Extension methods (#2)
    * Deleted folders (#1)
    
    * Delete Assignment3.Tests/bin directory
    
    * Delete Assignment3.Tests/obj directory
    
    * Delete Assignment3/bin directory
    
    * Delete Assignment3/obj directory
    
    * Add gitignore
    
    * Extensions
    
    Co-authored-by: MLFlexer <75012728+MLFlexer@users.noreply.github.com>
    Co-authored-by: Magnus Dalsgaard Larsen <daml@itu.dk>

--- Assignment3.Tests/ExtensionsTests.cs
+++ Assignment3.Tests/ExtensionsTests.cs
@@ -1,14 +1,37 @@
 using System;
 using Xunit;
 
-namespace BDSA2020.Assignment02.Tests
+namespace Assignment3.Tests
 {
     public class ExtensionsTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("https://example.foobar.com", true)]
+        [InlineData("http://example.foobar.com", false)]
+        public void IsSecureReturnsExpected (string str, bool excpected)
         {
+            // Arrange
+            var uri = new Uri(str);
 
+            // Act
+            var actual = uri.IsSecure();
+            
+            // Assert
+            Assert.Equal(excpected, actual);
+        }
+
+        [Theory]
+        [InlineData("splits the string at a delimiter determined by a regular expression instead of a set of characters. The string is split as many times as possible. If no delimiter is found, the return value contains one element whose value is the original", 42)]
+        [InlineData("abc 123 def", 2)]
+        [InlineData("abc123 def", 2)]
+        [InlineData("abc123def", 2)]
+        public void WordcountReturstExpected (string str, int expected)
+        {
+            // Act
+            var actual = str.WordCount();
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
