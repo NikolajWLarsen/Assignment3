a36c8c58948ddc628ca769757f5b57050412129a
Magnus Dalsgaard Larsen <daml@itu.dk>
2021-09-24 13:49:05 +0200

    Extensions

--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,8 +1,19 @@
 using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
-namespace BDSA2020.Assignment02
+namespace Assignment3
 {
     public static class Extensions
     {
+        public static bool IsSecure (this Uri uri)
+        {
+            return uri.Scheme == Uri.UriSchemeHttps;
+        }
+
+        public static int WordCount (this string str)
+        {
+            return Regex.Split(str, @"[^\p{L}]+").Count();
+        }
     }
 }
